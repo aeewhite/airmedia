dffa03774fbdc12e1ba7e58ee455b9e3f3b8c121
Andrew White <aeewhite@gmail.com>
2018-05-04 22:47:27 -0400

    Implement network requests to change airmedia background

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #[macro_use]
 extern crate quicli;
+extern crate indicatif;
+extern crate reqwest;
+
 use quicli::prelude::*;
 
-extern crate indicatif;
 use std::fs;
+use std::net::IpAddr;
 use std::path::PathBuf;
+use std::str::FromStr;
 use std::thread::sleep;
 use std::time::Duration;
-
-use std::net::IpAddr;
-use std::str::FromStr;
 
 use indicatif::{HumanBytes, ProgressBar, ProgressStyle};
 
@@ -34,8 +35,7 @@ main!(|args: Cli, log_level: verbosity| {
     let ip = IpAddr::from_str(&args.ip_addr)?;
     check_background_input(&args.background_file)?;
     login_to_airmedia(&args.auth, &ip)?;
-    upload_image(&args.background_file);
-    apply_settings();
+    upload_image(&ip, &args.background_file)?;
 });
 
 fn check_background_input(file: &PathBuf) -> Result<()> {
@@ -53,7 +53,7 @@ fn check_background_input(file: &PathBuf) -> Result<()> {
         bail!("Selected image is not a file");
     }
     // Size Check
-    let max_bytes = 7000000;
+    let max_bytes = 522240;
     if meta.len() > max_bytes {
         bail!("Image is larger than {:} limit", HumanBytes(max_bytes));
     }
@@ -76,6 +76,9 @@ fn login_to_airmedia(auth: &str, ip: &IpAddr) -> Result<()> {
     if let Some(user) = auth_parts.next() {
         if let Some(password) = auth_parts.next() {
             info!("Using authentication {:?}:{:?}", user, password);
+
+            let url = format!("https://{}/cgi-bin/login.cgi", ip);
+            debug!("Logging in with {:}", url);
             let sp = ProgressBar::new_spinner();
             sp.enable_steady_tick(100);
             sp.set_style(
@@ -85,6 +88,19 @@ fn login_to_airmedia(auth: &str, ip: &IpAddr) -> Result<()> {
             );
             sp.tick();
             sleep(Duration::from_secs(3));
+            let client = reqwest::Client::new();
+            let params = [
+                ("login", "admin"),
+                ("account", &user),
+                ("password", &password),
+            ];
+            // let _res = client
+            //     .post(&url)
+            //     .query(&[("lang", "en"), ("src", "AwLoginAdmin.html")])
+            //     .form(&params)
+            //     .send()?
+            //     .error_for_status()?;
+
             sp.finish_and_clear();
             println!("✔ Login Complete");
             return Ok(());
@@ -93,7 +109,9 @@ fn login_to_airmedia(auth: &str, ip: &IpAddr) -> Result<()> {
     bail!("Invalid username or password")
 }
 
-fn upload_image(file: &PathBuf) {
+fn upload_image(ip: &IpAddr, file: &PathBuf) -> Result<()> {
+    let url = format!("https://{}/cgi-bin/web_index.cgi", ip);
+    debug!("Posting image to {:}", url);
     let sp = ProgressBar::new_spinner();
     sp.enable_steady_tick(100);
     sp.set_style(
@@ -103,20 +121,17 @@ fn upload_image(file: &PathBuf) {
     );
     sp.tick();
     sleep(Duration::from_secs(3));
-    sp.finish_and_clear();
-    println!("✔ Image Uploaded");
-}
+    let client = reqwest::Client::new();
+    let form = reqwest::multipart::Form::new().file("filename", &file)?;
+    let mut _result = client
+        .post(&url)
+        .query(&[("lang", "en"), ("src", "AwOsdTool.html")])
+        .multipart(form)
+        .send()?
+        .error_for_status()?;
 
-fn apply_settings() {
-    let sp = ProgressBar::new_spinner();
-    sp.enable_steady_tick(100);
-    sp.set_style(
-        ProgressStyle::default_spinner()
-            .tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇")
-            .template("{spinner} Changing background setting"),
-    );
-    sp.tick();
-    sleep(Duration::from_secs(3));
     sp.finish_and_clear();
-    println!("✔ Settings Updated");
+    debug!("Response {:}", _result.text()?);
+    println!("✔ Image Uploaded");
+    Ok(())
 }
